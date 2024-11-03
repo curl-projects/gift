78a8425ab170c87d22391d83596fccf3bc9d8eb8
finnianmacken <finn@minerva.kgi.edu>
2024-11-03 08:50:16 -0800

    implemented grid layout instead of table for antialiasing/

--- app/components/canvas/shapes/journal-shape/journal-covenants/clause-cards/justify-card/ExpandedJustifyCard.jsx
+++ app/components/canvas/shapes/journal-shape/journal-covenants/clause-cards/justify-card/ExpandedJustifyCard.jsx
@@ -3,74 +3,57 @@ import styles from './ExpandedJustifyCard.module.css';
 import { EditorContent, useEditor } from '@tiptap/react';
 import { Node } from "@tiptap/core";
 import StarterKit from '@tiptap/starter-kit';
-
 import { Heading } from '@tiptap/extension-heading';
 import { Paragraph } from '@tiptap/extension-paragraph';
 import { Text } from '@tiptap/extension-text';
 import Link from "@tiptap/extension-link";
-import Placeholder from '@tiptap/extension-placeholder'
+import Placeholder from '@tiptap/extension-placeholder';
 import * as showdown from 'showdown';
-import { CovenantConjunction, CovenantClause } from "~/components/canvas/custom-ui/utilities/ConstellationLabelPainter.jsx"
+import { CovenantConjunction, CovenantClause } from "~/components/canvas/custom-ui/utilities/ConstellationLabelPainter.jsx";
 
-export function ExpandedJustifyCard({ modifier, currentCount = 1, titleScale=0.6}) {
+export function ExpandedJustifyCard({ modifier, currentCount = 1, titleScale=0.6 }) {
     const [htmlContent, setHtmlContent] = useState("");
     const converter = new showdown.Converter();
 
     const editor = useEditor({
         extensions: [
-        StarterKit.configure({
-            heading: true,
-            paragraph: true
+            StarterKit.configure({
+                heading: true,
+                paragraph: true
             }),
-          // ColorHighlighter.configure({ 
-          //   data: ['Hello'],
-          //   tldrawEditor: tldrawEditor,
-          // }),
-          //   CustomHeading,
-          //   CustomParagraph,
-          Link,
-          Placeholder.configure({
-            placeholder: "Capture your thoughts",
-            showOnlyWhenEditable: false,
-            emptyNodeClass: styles.isEmpty,
-            emptyEditorClass: styles.empty,
-        }),
+            Link,
+            Placeholder.configure({
+                placeholder: "Capture your thoughts",
+                showOnlyWhenEditable: false,
+                emptyNodeClass: styles.isEmpty,
+                emptyEditorClass: styles.empty,
+            }),
         ],
         content: htmlContent,
         editable: true,
-        onUpdate: ({ editor }) => {
-        },
-        onSelectionUpdate: ({ editor }) => {
-        }
-    })
+    });
 
     useEffect(() => {
         if (editor) {
-          editor.commands.setContent(htmlContent);
-        //   editor.commands.focus(); // Automatically focus the editor
+            editor.commands.setContent(htmlContent);
         }
-      }, [htmlContent, editor]);
+    }, [htmlContent, editor]);
 
     return (
-        <div style={{
-            height: '100%',
+        <div className={styles.expandedJustifyCard} style={{
+            display: 'grid',
+            gridTemplateRows: '1fr',
+            gridTemplateColumns: '1fr',
             width: '100%',
-            overflow: 'scroll',
+            height: '100%',
+            border: '2px solid black',
+            padding: '20px',
         }}>
-            <div className={styles.expandedJustifyCard} style={{
-                // this is completely necessary to avoid subpixel antialiasing issues with chrome and other wekbit browsers
-                display: 'table', 
-                tableLayout: 'fixed',
-                width: '100%',
-                height: '100%',
-                border: '2px solid black',
-                // overflow: 'scroll',
-                
-            }}>
+            <div className={styles.gridController}>
                 <p className={styles.clauseTitleContainer} style={{
                     transform: `scale(${titleScale})`,
-                    width: `${100/titleScale}%`,
                     transformOrigin: 'top left',
+                    width: `${100/titleScale}%`,
                 }}>
                     <CovenantConjunction modifier={modifier} />
                     <CovenantClause modifier={modifier} currentCount={currentCount} />
@@ -78,11 +61,8 @@ export function ExpandedJustifyCard({ modifier, currentCount = 1, titleScale=0.6
                 <EditorContent 
                     editor={editor} 
                     className={styles.justifyCardEditor}
-                    style={{
-                        transform: `perspective(1px)`,
-                    }}
                 />
             </div>
         </div>
-    )
+    );
 }
